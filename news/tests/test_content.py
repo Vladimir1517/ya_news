c03b6649486cd31cbc8a72c4f9149c74cdc0547e
Владимир Тютин <vladimir30061517@gmail.com>
2023-11-16 16:54:29 +0300

    Ver_1

---
+++ news/tests/test_content.py
@@ -0,0 +1,143 @@
+from datetime import datetime, timedelta
+from http import HTTPStatus
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from news.models import Comment, News
+from news.forms import BAD_WORDS, WARNING
+
+User = get_user_model()
+
+
+class TestContent(TestCase):
+
+    HOME_URL = reverse('news:home')
+
+    @classmethod
+    def setUpTestData(cls):
+        today = datetime.today()
+        all_news = [
+            News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                # Для каждой новости уменьшаем дату на index дней от today,
+                # где index - счётчик цикла.
+                date=today - timedelta(days=index)
+            )
+            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+        ]
+        News.objects.bulk_create(all_news)
+
+    def test_news_count(self):
+        # Загружаем главную страницу.
+        response = self.client.get(self.HOME_URL)
+        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
+        # Получаем список объектов из словаря контекста.
+        object_list = response.context['object_list']
+        # Определяем длину списка.
+        news_count = len(object_list)
+        # Проверяем, что на странице именно 10 новостей.
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
+
+    def test_news_order(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        # Получаем даты новостей в том порядке, как они выведены на странице.
+        all_dates = [news.date for news in object_list]
+        # Сортируем полученный список по убыванию.
+        sorted_dates = sorted(all_dates, reverse=True)
+        # Проверяем, что исходный список был отсортирован правильно.
+        self.assertEqual(all_dates, sorted_dates)
+
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(
+            title='Тестовая новость', text='Просто текст.'
+        )
+        # Сохраняем в переменную адрес страницы с новостью:
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        # Запоминаем текущее время:
+        now = timezone.now()
+        # Создаём комментарии в цикле.
+        for index in range(2):
+            # Создаём объект и записываем его в переменную.
+            comment = Comment.objects.create(
+                news=cls.news, author=cls.author, text=f'Tекст {index}',
+            )
+            # Сразу после создания меняем время создания комментария.
+            comment.created = now + timedelta(days=index)
+            # И сохраняем эти изменения.
+            comment.save()
+
+    def test_comments_order(self):
+        response = self.client.get(self.detail_url)
+        # Проверяем, что объект новости находится в словаре контекста
+        # под ожидаемым именем - названием модели.
+        self.assertIn('news', response.context)
+        # Получаем объект новости.
+        news = response.context['news']
+        # Получаем все комментарии к новости.
+        all_comments = news.comment_set.all()
+        # Проверяем, что время создания первого комментария в списке
+        # меньше, чем время создания второго.
+        self.assertLess(all_comments[0].created, all_comments[1].created)
+
+    def test_anonymous_client_has_no_form(self):
+        response = self.client.get(self.detail_url)
+        self.assertNotIn('form', response.context)
+
+    def test_authorized_client_has_form(self):
+        # Авторизуем клиент при помощи ранее созданного пользователя.
+        self.client.force_login(self.author)
+        response = self.client.get(self.detail_url)
+        self.assertIn('form', response.context)
+
+
+class TestCommentCreation(TestCase):
+    # Текст комментария понадобится в нескольких местах кода,
+    # поэтому запишем его в атрибуты класса.
+    COMMENT_TEXT = 'Текст комментария'
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+        # Адрес страницы с новостью.
+        cls.url = reverse('news:detail', args=(cls.news.id,))
+        # Создаём пользователя и клиент, логинимся в клиенте.
+        cls.user = User.objects.create(username='Мимо Крокодил')
+        cls.auth_client = Client()
+        cls.auth_client.force_login(cls.user)
+        # Данные для POST-запроса при создании комментария.
+        cls.form_data = {'text': cls.COMMENT_TEXT}
+
+    def test_anonymous_user_cant_create_comment(self):
+        # Совершаем запрос от анонимного клиента, в POST-запросе отправляем
+        # предварительно подготовленные данные формы с текстом комментария.
+        self.client.post(self.url, data=self.form_data)
+        # Считаем количество комментариев.
+        comments_count = Comment.objects.count()
+        # Ожидаем, что комментариев в базе нет - сравниваем с нулём.
+        self.assertEqual(comments_count, 0)
+
+    def test_user_can_create_comment(self):
+        # Совершаем запрос через авторизованный клиент.
+        response = self.auth_client.post(self.url, data=self.form_data)
+        # Проверяем, что редирект привёл к разделу с комментами.
+        self.assertRedirects(response, f'{self.url}#comments')
+        # Считаем количество комментариев.
+        comments_count = Comment.objects.count()
+        # Убеждаемся, что есть один комментарий.
+        self.assertEqual(comments_count, 1)
+        # Получаем объект комментария из базы.
+        comment = Comment.objects.get()
+        # Проверяем, что все атрибуты комментария совпадают с ожидаемыми.
+        self.assertEqual(comment.text, self.COMMENT_TEXT)
+        self.assertEqual(comment.news, self.news)
+        self.assertEqual(comment.author, self.user)
--- news/tests/test_routes.py
+++ news/tests/test_routes.py
@@ -1,32 +1,11 @@
 from http import HTTPStatus
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-
 from news.models import Comment, News
 
 User = get_user_model()
-
-
-class TestRoutes(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cls.news = News.objects.create(title='Заголовок', text='Текст')
-
-    def test_pages_availability(self):
-        urls = (
-            ('news:home', None),
-            ('news:detail', (self.news.id,)),
-            ('users:login', None),
-            ('users:logout', None),
-            ('users:signup', None),
-        )
-        for name, args in urls:
-            with self.subTest(name=name):
-                url = reverse(name, args=args)
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
 
 
 class TestRoutes(TestCase):
@@ -43,6 +22,20 @@ class TestRoutes(TestCase):
             author=cls.author,
             text='Текст комментария'
         )
+
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_availability_for_comment_edit_and_delete(self):
         users_statuses = (
